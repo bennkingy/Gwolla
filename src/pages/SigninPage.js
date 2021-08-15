296fde119b501f54a13fe4947c52d81503aec0d5
Benn King <bennkingy@gmail.com>
2021-08-15 23:24:00 +0100

    added routes and finished sign in page

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
+
+import SignIn from '../components/SignIn';
 
 const SignInPage = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
-
   return (
     <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
-      <h1>SignIn Page</h1>
+      <SignIn />
     </>
-  )
-}
+  );
+};
+
 
 export default SignInPage
