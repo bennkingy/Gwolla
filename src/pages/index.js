4fbd155323a79f8074038e17ca09277a2fd16b18
Benn King <bennkingy@gmail.com>
2021-08-15 23:31:11 +0100

    Footer added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import NavBar from '../components/Navbar'
 import HeroSection from "../components/HeroSection"
 import InfoSection from "../components/InfoSection"
 import Services from '../components/Services'
+import Footer from '../components/Footer'
 
 import {
   homeObjOne,
@@ -28,6 +29,7 @@ const Home = () => {
       <InfoSection {...homeObjTwo} />
       <Services />
       <InfoSection {...homeObjThree} />
+      <Footer />
     </>
   )
 }
