cafc44583a2217f9037b2a61fb85e8d2dd21c255
Benn King <bennkingy@gmail.com>
2021-08-15 21:59:30 +0100

    hero section added

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper,  ArrowForward,
+  ArrowRight, } from './HeroElements'
+import { Button } from '../ButtonElements';
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <div>
       <HeroContainer>
-        
+        <HeroBg>
+          <VideoBg autoPlay loop muted src={Video} type='video/mp4'></VideoBg>
+        </HeroBg>
+        <HeroContent>
+          <HeroH1>
+            Virtual Banking Made Easy
+          </HeroH1>
+          <HeroP>
+            Sign up for a new account today and receive £250 in credit towards your next payment.
+          </HeroP>
+        </HeroContent>
+        <HeroBtnWrapper>
+        <Button
+          to='signup'
+          onMouseEnter={onHover}
+          onMouseLeave={onHover}
+          primary='true'
+          dark='true'
+          offset={-80}
+        >
+          Get started {hover ? <ArrowForward /> : <ArrowRight />}
+        </Button>
+        </HeroBtnWrapper>
       </HeroContainer>
     </div>
   )
